2673d3e56ce688b9b5fc46d10ff93283a1fa6e76
mohammad-damra <mohammad.s.damra@gmail.com>
2020-11-28 13:17:57 +0200

    after the changes

--- back-end/controllers/main-controller.js
+++ back-end/controllers/main-controller.js
@@ -1,11 +1,9 @@
 const connection = require('../db');
 
 const getAllOrders = (req, res) => {
-	//soft delete active
-	//const query = `SELECT * FROM orders where is_deleted = ?`;
-	//const data = [orders.is_deleted];
-	const query = `SELECT * FROM orders`;
-	connection.query(query, (err, result) => {
+	const query = `SELECT * FROM orders where is_deleted = ?`;
+	const data = [ orders.is_deleted ];
+	connection.query(query, data, (err, result) => {
 		if (err) throw err;
 		res.json(result);
 	});
@@ -22,21 +20,12 @@ const addNewOrder = (req, res) => {
 };
 
 const deleteOrderById = (req, res) => {
-	/*
-	--soft delete--
 	const query = `UPDATE orders 
     SET 
     is_deleted = ?
     where
 	ordersId= ?`;
-	const data = [orders.is_deleted,orders.ordersId];
-	connection.query(query,data, (err, result) => {
-		if (err) throw err;
-		res.json(result);
-	});
-	*/
-	const query = `DELETE FROM orders WHERE ordersId = ? ;`;
-	const data = [ orders.ordersId ];
+	const data = [ orders.is_deleted, orders.ordersId ];
 	connection.query(query, data, (err, result) => {
 		if (err) throw err;
 		res.json(result);
--- back-end/routes/main-route.js
+++ back-end/routes/main-route.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const mainRouter = express.Router();
-const {getAllOrders,addNewOrder,deleteOrderById} = require("../controllers/main-controller");
-
-mainRouter.get("/orders", getAllOrders);
-mainRouter.post("/orders", addNewOrder);
-mainRouter.delete("/orders", deleteOrderById);
-
-
+const { getAllOrders, addNewOrder, deleteOrderById } = require('../controllers/main-controller');
+mainRouter.get('/orders', getAllOrders);
+mainRouter.post('/orders', addNewOrder);
+mainRouter.delete('/orders', deleteOrderById);
 module.exports = mainRouter;
