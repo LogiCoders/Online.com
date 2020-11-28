79cf736bddb2c713ce327bbe0748618ecd700802
malek88888 <jmzomalek@gmail.com>
2020-11-28 16:11:42 +0200

    the files are edited test

--- back-end/routes/main-route.js
+++ back-end/routes/main-route.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mainRouter = express.Router();
-const {addProduct,getproducts,deleteProduct,updateProduct,addStore,updateStore,getStores,deleteStore,
-  createItem,deleteItem,createOrder,getItems,getOrders}=require("../controllers/main-controller")
-const {register}=require("../controllers/users_controller")
+const { addProduct, getproducts, deleteProduct, updateProduct, addStore, updateStore, getStores, deleteStore,
+  createItem, deleteItem, createOrder, getItems, getOrders } = require("../controllers/main-controller")
+const { register } = require("../controllers/users_controller")
 
 mainRouter.get('/', (req, res) => {
   res.json('HELLO WORLD');
